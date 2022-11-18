d59c1bb0fbacc76a524c1fde31776716ab51b6b2
cesargaleano <87844938+cesargaleano@users.noreply.github.com>
2022-11-18 00:55:25 -0500

    Update

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const express = require('express');
 //const fileUpload = require('express-fileupload');
 const server = express();
 //const CORS = require('cors');
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 //const authRoute = require('./routes/auth');
 //const userRoute = require('./routes/user');
 //const cartRoute = require('./routes/cart');
-//const productRoute = require('./routes/product');
+const productRoute = require('./routes/product');
 //const orderRoute = require('./routes/order');
 //const fileRoute = require('./routes/file');
 
 const PORT = process.env.PORT || 8080;
 
 dotenv.config();
-//mongoose.connect(process.env.MONGO_URL)
-//        .then(()=>console.log('Bd is connected'))
-//        .catch((e)=>console.log(e)); 
+mongoose.connect(process.env.MONGO_URL)
+        .then(()=>console.log('Bd is connected'))
+        .catch((e)=>console.log(e)); 
 
 /*server.use(CORS({
         origin: '*',
@@ -29,7 +29,7 @@ server.use(express.json());
 //server.use('/api/auth',authRoute);
 //server.use('/api/user',userRoute);
 //server.use('/api/cart',cartRoute);
-//server.use('/api/product',productRoute);
+server.use('/api/product',productRoute);
 //server.use('/api/order',orderRoute);
 //server.use('/api/store/file',fileRoute);
 
